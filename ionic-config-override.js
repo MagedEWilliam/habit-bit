da9d63915a5cb7b75fc6934b55405ed490012d44
Maajid E. William <Maged.EWilliam@Gmail.com>
2020-10-25 02:55:10 +0200

    add _redirects as it's the recomended way

--- ionic-config-override.js
+++ ionic-config-override.js
@@ -1,3 +1,4 @@
 var fs = require('fs-extra');
-fs.copy('src/404.html','www/404.html');
-console.log('>>>>>> Move 404.html to www');
+
+fs.copy('src/_redirects','www/_redirects');
+console.log('[00:00.0]  Move _redirects to www');
