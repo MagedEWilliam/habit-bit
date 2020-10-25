44896893e2a052e860e09f12f82883506ae254e5
Maajid E. William <Maged.EWilliam@Gmail.com>
2020-10-25 22:15:19 +0200

    SPA SW route, add purpose to image

--- src/sw-base.js
+++ src/sw-base.js
@@ -1,2 +1,18 @@
 importScripts('./workbox-v5.1.4/workbox-sw.js')
+
+workbox.routing.registerRoute(
+    ({ event }) => event.request.mode === 'navigate',
+    async () => {
+        console.log('im here')
+      const defaultBase = '/index.html';
+      return caches
+        .match(workbox.precaching.getCacheKeyForURL(defaultBase))
+        .then(response => {
+            return response || fetch(defaultBase);
+        })
+        .catch(err => {
+          return fetch(defaultBase);
+        });
+    }
+  );
 workbox.precaching.precacheAndRoute(self.__WB_MANIFEST);
