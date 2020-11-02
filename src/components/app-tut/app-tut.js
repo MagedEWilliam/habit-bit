89d498a0747ca5808bb8b6024f094ee4e3c607f8
Maajid E. William <Maged.EWilliam@Gmail.com>
2020-11-02 14:05:36 +0200

    fix tutorial

--- src/components/app-tut/app-tut.js
+++ src/components/app-tut/app-tut.js
@@ -21,27 +21,33 @@ export class AppTabs {
           <ion-slide pager={true} options={this.slideOpts}>
             <div class="slide"> 
               <img src="/assets/a0c46248-b1d5-4a80-bcd4-8def270a1fda.gif"/>
-              <h2>Welcome</h2>
-              <p>Have you seen this project before?</p>
-              <p>This app give you the same function</p>
+              <span>
+                <h2>Welcome</h2>
+                <p>Have you seen this project before?</p>
+                <p>This app give you the same function</p>
+              </span>
             </div>
           </ion-slide>
 
           <ion-slide>
             <div class="slide">
               <img src="/assets/a0c46248-b1d5-4a80-bcd4-8def270a1fda.gif"/>
-              <h2>Create a new habit</h2>
-              <p>Everyday mark today on the calendar.</p>
-              <p>At the end of the year you'll be able to track your habits</p>
+              <span>
+                <h2>Create a new habit</h2>
+                <p>Everyday mark today on the calendar.</p>
+                <p>At the end of the year you'll be able to track your habits</p>
+              </span>
             </div>
           </ion-slide>
 
           <ion-slide>
             <div class="slide">
-              <img src="/assets/118549043_2011659842298798_5545103259183867879_n.jpg"/>
-              <h2>Finally</h2>
-              <p>This project is inspired by <a href="https://www.kickstarter.com/projects/simonegiertz/the-every-day-calendar">this project</a> by Simon Giertz.</p>
-              <ion-button size="large" href="/new-habit/">Start</ion-button>
+              <img  src="/assets/118549043_2011659842298798_5545103259183867879_n.jpg"/>
+              <span>
+                <h2>Finally</h2>
+                <p>This project is inspired by <a href="https://www.kickstarter.com/projects/simonegiertz/the-every-day-calendar">this project</a> by Simon Giertz.</p>
+                <ion-button size="large" href="/new-habit/">Start</ion-button>
+              </span>
             </div>
           </ion-slide>
 
